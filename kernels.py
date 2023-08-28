b9229eccc6b22cd3cb61eeee60d4fa8865eae0fb
grastagg <grastagg33@gmail.com>
2023-08-28 13:38:51 -0600

    working on radar kernel function

---
+++ kernels.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def radial_kernel(x1, x2, center, sigma_f, length_scale):
+    return sigma_f * np.exp(-(np.linalg.norm(x1-center)-np.linalg.norm(x2-center))**2/length_scale)
+
+    
+def create_K_matrix(x1_vec, x2_vec, center, sigma_f, length_scale):
+    K = np.zeros((len(x1_vec),len(x2_vec)))
+    
+    for i in range(len(x1_vec)):
+        for j in range(len(x2_vec)):
+            K[i,j] = radial_kernel(x1_vec[i],x2_vec[j], center, sigma_f, length_scale)
+    
+    return K
+
+
+if __name__ == '__main__':
+    num_points = 1000
+    x_test = np.linspace(0,1200,num_points)
+    y_test = np.linspace(0,1200,num_points)
+
+    [X_test, Y_test] = np.meshgrid(x_test, y_test)
+    kernel_value = np.zeros_like(X_test)
+    center = np.array([[600,600]])
+    sigma_f = 1
+    length_scale = 1000
+
+    measurement_location = np.array([[600,10]])
+    
+    for i in range(num_points):
+        for j in range(num_points):
+            kernel_value[i,j] = radial_kernel(np.array([[X_test[i,j],Y_test[i,j]]]), measurement_location,center,sigma_f,length_scale)
+            
+    
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    c = ax.pcolormesh(X_test, Y_test, kernel_value)
+    plt.colorbar(c)
+    plt.show()
+    
+    
