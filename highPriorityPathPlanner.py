36f5515daa41881cd5f7429b4bb4dfd9c90c34e7
ggs24 <grastagg33@gmail.com>
2024-05-16 16:02:26 -0600

    working on bspline code for jax

--- highPriorityPathPlanner.py
+++ highPriorityPathPlanner.py
@@ -26,6 +26,9 @@ import igraph as ig
 import time
 
 
+from bspline.bsplines import BsplineEvaluation
+
+
 class HighPriorityPathPlannerDeterministic:
     def __init__(self) -> None:
         pass
@@ -64,8 +67,11 @@ class HighPriorityPathPlannerDeterministic:
 
 
     def get_turn_rate_and_velocity(self, t, spl):
-        out_d1 = spl.derivative(1)(t)
-        out_d2 = spl.derivative(2)(t)
+        # out_d1 = spl.derivative(1)(t)
+        # out_d2 = spl.derivative(2)(t)
+        out_d1 = spl.get_spline_derivative_data(len(t),1)
+        out_d2 = spl.get_spline_derivative_data(len(t),2)
+        
         x1_dot = out_d1[:,0]
         x2_dot = out_d1[:,1]
         x1_ddot = out_d2[:,0]
@@ -112,7 +118,8 @@ class HighPriorityPathPlannerDeterministic:
 
 
         #create scipy bpline object
-        spline = interpolate.BSpline(t, control_points, 3)
+        # spline = interpolate.BSpline(t, control_points, 3)
+        spline = BsplineEvaluation(control_points, t, 3,clamped=True)
 
         return spline
 
